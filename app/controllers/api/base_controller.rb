a168bb54cdf7d3568988de0d8b8bb334340ec5e0
behemoth-truongnd <truongnd@behemoth.vn>
2021-02-24 14:58:58 +0700

    fix handle error

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -5,14 +5,8 @@ module Api
 
     rescue_from ActionController::RoutingError, with: :render404
     rescue_from ActiveRecord::RecordNotFound, with: :render404
-    rescue_from StandardError, with: :render500
 
-    def render500(exception)
-      write_log(exception)
-      render status: 500, json: { errors: { message: "Server error" } }
-    end
-
-    def render404
+    def render404(exception)
       write_log(exception)
       render status: 404, json: { errors: { message: "Not Found" } }
     end
