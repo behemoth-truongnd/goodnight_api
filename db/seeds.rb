331782fbeb6e70e5efb301c1983f567ee666d413
behemoth-truongnd <truongnd@behemoth.vn>
2021-02-24 09:02:35 +0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create users
+puts "User Creating..."
+user_attributes = 5.times.map do |i|
+  { name: "Test user #{i + 1}" }
+end
+User.import(user_attributes)
+puts "User Created"
+
+# Create user_followers
+puts "UserFollowers Creating..."
+user_ids = User.pluck(:id)
+user_follows = user_ids.map do |user_id|
+  { user_id: user_id, follower_id: (user_ids - [user_id]).sample }
+end
+UserFollower.import(user_follows)
+puts "UserFollower Created"
+
+# Create time_tracks
+puts "TimeTrack Creating..."
+user_ids.each do |user_id|
+  time_tracks = 5.times.map do |i|
+    sleep_at = Time.current.beginning_of_day - (i + 1).days + (20..23).to_a.sample.hours + (0..60).to_a.sample.minutes
+    { user_id: user_ids, sleep_at: sleep_at, wakeup_at: sleep_at + (7..9).to_a.sample.hours + (0..60).to_a.sample.minutes }
+  end
+  TimeTrack.import time_tracks
+end
+puts "TimeTrack Created"
